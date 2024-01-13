615a7bb6f799160b4d8d5bcbef930bd9465819af
Leronus <leroy.de.smet@hva.nl>
2024-01-13 05:56:53 +0100

    Fabric initial commit

---
+++ src/main/java/mod/leronus/mores/MoresDataGenerator.java
@@ -0,0 +1,11 @@
+package mod.leronus.mores;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MoresDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
